54bb8f97fc66f026fd89e20d37082d86b94de0a0
Pierre Tardy <tardyp@gmail.com>
2010-05-13 18:28:48 +0200

    examples: correct typo
    Signed-off-by: Pierre Tardy <tardyp@gmail.com>

--- examples/start.sh
+++ examples/start.sh
@@ -6,7 +6,7 @@ cd /sys/kernel/debug/tracing
 echo sched:sched_wakeup > set_event
 echo sched:sched_switch >> set_event
 echo workqueue:workqueue_execution >> set_event
-echo power: >>set_event
-echo irq: >>set_event
+echo power:* >>set_event
+echo irq:* >>set_event
 echo >trace
 echo 1 >tracing_enabled
