c65298620c3d9ea657e5888ecba5ebc79d765dd1
Pierre Tardy <pierre.tardy@intel.com>
2010-12-01 01:28:04 +0100

    spi: spi plugin
    that displays spi activities in the timechart
    
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

---
+++ timechart/plugins/spi.py
@@ -0,0 +1,30 @@
+from timechart.plugin import *
+from timechart import colors
+from timechart.model import tcProcess
+
+# to use with start_spi.sh
+
+class spi(plugin):
+    additional_colors = """
+spi_bg		      #80ff80
+"""
+    additional_ftrace_parsers = [
+        ]
+    additional_process_types = {
+            "spi":(tcProcess, MISC_TRACES_CLASS),
+        }
+    @staticmethod
+    def do_event_spi_sync(proj,event):
+        process = proj.generic_find_process(0,"spi:%s"%(event.caller),"spi")
+        proj.last_spi.append(process)
+        proj.generic_process_start(process,event,False)
+    @staticmethod
+    def do_event_spi_complete(proj,event):
+        process = proj.last_spi.pop(0)
+        proj.generic_process_end(process,event,False)
+    @staticmethod
+    def do_event_spi_async(proj,event):
+        if event.caller != 'spi_sync':
+            proj.do_event_spi_sync(event,False)
+
+plugin_register(spi)
